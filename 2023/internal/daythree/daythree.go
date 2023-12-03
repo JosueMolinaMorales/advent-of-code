8b4572264322fee53ebf76b1b006434e79c3ac6b
Josue Morales <molinajosue92@hotmail.com>
2023-12-03 09:19:52 -0500

    setup for day 3

---
+++ 2023/internal/daythree/daythree.go
@@ -0,0 +1,23 @@
+package daythree
+
+var testInput string = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+func RunDayThree() {
+}
+
+func partOne(input string) int {
+	return 0
+}
+
+func partTwo(input string) int {
+	return 0
+}
