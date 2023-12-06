9dc9d40cb127249ac6baff7232db6238e1bbab28
Josue Morales <molinajosue92@hotmail.com>
2023-12-06 01:10:15 -0500

    finish day 6

--- 2023/internal/daysix/daysix.go
+++ 2023/internal/daysix/daysix.go
@@ -2,6 +2,8 @@ package daysix
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -15,10 +17,18 @@ type TimeDistance struct {
 }
 
 func RunDaySix() {
-	partOne(testInput)
+	input, err := os.ReadFile("./input/day6input.txt")
+	if err != nil {
+		panic("Failed to read file for day 6")
+	}
+
+	res := partOne(string(input))
+	fmt.Println("Part 1:", res)
+	res = partTwo(string(input))
+	fmt.Println("Part 2:", res)
 }
 
-func partOne(input string) {
+func partOne(input string) int {
 	times := make([]int, 0)
 	dists := make([]int, 0)
 	for i, line := range strings.Split(input, "\n") {
@@ -45,7 +55,7 @@ func partOne(input string) {
 
 	ans := 1
 	for _, td := range timeDists {
-		half := td.Time / 2
+		half := int(math.Floor(float64(td.Time) / 2.0))
 		count := 0
 
 		time := half * (td.Time - half)
@@ -55,7 +65,51 @@ func partOne(input string) {
 			time = half * (td.Time - half)
 		}
 
-		ans *= count
+		half = int(math.Floor(float64(td.Time)/2.0)) + 1
+		time = half * (td.Time - half)
+		for time > td.Distance {
+			count += 1
+			half += 1
+			time = half * (td.Time - half)
+		}
+
+		ans = ans * count
 	}
-	fmt.Println(ans)
+
+	return ans
+}
+
+func partTwo(input string) int {
+	lines := strings.Split(input, "\n")
+	timeLine := strings.ReplaceAll(strings.ReplaceAll(lines[0], "Time: ", ""), " ", "")
+
+	time, err := strconv.Atoi(timeLine)
+	if err != nil {
+		fmt.Println("Failed to convert", timeLine, "to int")
+	}
+
+	distStr := strings.ReplaceAll(strings.ReplaceAll(lines[1], "Distance: ", ""), " ", "")
+	dist, err := strconv.Atoi(distStr)
+	if err != nil {
+		fmt.Println("Failed to convert", distStr, "to int")
+	}
+
+	count := 0
+	half := int(math.Floor(float64(time) / 2.0))
+	t := half * (time - half)
+	for t > dist {
+		count += 1
+		half -= 1
+		t = half * (time - half)
+	}
+
+	half = int(math.Floor(float64(time)/2.0)) + 1
+	t = half * (time - half)
+	for t > dist {
+		count += 1
+		half += 1
+		t = half * (time - half)
+	}
+
+	return count
 }
