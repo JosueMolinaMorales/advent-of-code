6f3bf503df7c51ad190d0824bb7aa1064a4cda7c
Josue Morales <molinajosue92@hotmail.com>
2023-12-09 22:48:46 -0500

    finish day 9

--- 2023/internal/nine/daynine.go
+++ 2023/internal/nine/daynine.go
@@ -2,8 +2,11 @@ package nine
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/josuemolinamorales/aoc-2023/utils/iterators"
 )
 
 const testInput = `0 3 6 9 12 15
@@ -11,10 +14,15 @@ const testInput = `0 3 6 9 12 15
 10 13 16 21 30 45`
 
 func RunDayNine() {
-	partOne(testInput)
+	input, err := os.ReadFile("./input/day9.txt")
+	if err != nil {
+		panic("Failed to read day 9 file")
+	}
+	fmt.Println("Part 1: ", partOne(string(input)))
+	fmt.Println("Part 2: ", partTwo(string(input)))
 }
 
-func partOne(input string) int {
+func parseInput(input string) [][]int {
 	numbers := make([][]int, 0)
 	for _, line := range strings.Split(input, "\n") {
 		nums := make([]int, 0)
@@ -27,20 +35,46 @@ func partOne(input string) int {
 		}
 		numbers = append(numbers, nums)
 	}
-
-	fmt.Println(numbers)
-
-	// Find the sequence then return the next number
-	for _, nums := range numbers {
-		fmt.Println(findSequence(nums))
-	}
-	return 0
+	return numbers
 }
 
-func findSequence(numbers []int) int {
-	return 0
+func partOne(input string) int {
+	numbers := parseInput(input)
+
+	// Find the sequence then return the next number
+	sum := 0
+	for _, nums := range numbers {
+		sum += findSequence(nums, 1)
+	}
+	return sum
 }
 
 func partTwo(input string) int {
-	return 0
+	numbers := parseInput(input)
+
+	sum := 0
+	for _, nums := range numbers {
+		sum += findSequence(nums, 2)
+	}
+
+	return sum
+}
+
+func findSequence(numbers []int, part int) int {
+	if iterators.Every(numbers, func(n int) bool { return n == 0 }) {
+		if part == 1 {
+			return numbers[len(numbers)-1]
+		}
+		return numbers[0]
+	}
+	differences := make([]int, 0)
+	for i := 0; i < len(numbers)-1; i++ {
+		differences = append(differences, (numbers[i+1] - numbers[i]))
+	}
+	diff := findSequence(differences, part)
+
+	if part == 1 {
+		return diff + numbers[len(numbers)-1]
+	}
+	return numbers[0] - diff
 }
