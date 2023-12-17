6372bb597ea4a6b9856fea0701aa1f691134b0ec
Josue Morales <molinajosue92@hotmail.com>
2023-12-17 15:54:08 -0500

    finish day 17 p1

--- 2023/internal/seventeen/day17.go
+++ 2023/internal/seventeen/day17.go
@@ -3,6 +3,8 @@ package seventeen
 import (
 	"container/heap"
 	"fmt"
+	"math"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
@@ -34,9 +36,25 @@ const test = `111
 551`
 
 func RunDaySeventeen() {
-	// input, _ := os.ReadFile("./input/day17.txt")
-	// fmt.Println("Part 1", partOne(string(input))) // 1139 too high
-	fmt.Println("Part 1", partOne(testInput))
+	input, _ := os.ReadFile("./input/day17.txt")
+	fmt.Println("Part 1", partOne(string(input)))
+	// fmt.Println("Part 1", partOne(testInput))
+}
+
+func partTwo(input string) int {
+	m := make([][]int, 0)
+	for _, line := range strings.Split(input, "\n") {
+		row := make([]int, 0)
+		for _, c := range line {
+			num, _ := strconv.Atoi(string(c))
+			row = append(row, num)
+		}
+		m = append(m, row)
+	}
+
+	minHeatLoss := minHeatLossDijkstra(m, 3, 10)
+
+	return minHeatLoss
 }
 
 func partOne(input string) int {
@@ -50,10 +68,9 @@ func partOne(input string) int {
 		m = append(m, row)
 	}
 
-	minHeatLoss := minHeatLossDijkstra(m)
-	fmt.Println(minHeatLoss)
+	minHeatLoss := minHeatLossDijkstra(m, 0, 3)
 
-	return 0
+	return minHeatLoss
 }
 
 type Point struct {
@@ -73,7 +90,7 @@ func (pq PriorityQueue) Less(i, j int) bool {
 
 func (pq PriorityQueue) Get(p *Point) int {
 	for i, point := range pq {
-		if point.row == p.row && point.col == p.col {
+		if point.row == p.row && point.col == p.col && point.prev.row == p.prev.row && point.prev.col == p.prev.col && point.numStraightMoves == p.numStraightMoves {
 			return i
 		}
 	}
@@ -97,10 +114,10 @@ func key(p *Point) string {
 	if prev == nil {
 		prev = &Point{-1, -1, -1, nil, -1}
 	}
-	return fmt.Sprintf("%d,%d,%d,%d", p.row, p.col, prev.row, prev.col)
+	return fmt.Sprintf("%d,%d,%d,%d,%d", p.row, p.col, prev.row, prev.col, p.numStraightMoves)
 }
 
-func minHeatLossDijkstra(matrix [][]int) int {
+func minHeatLossDijkstra(matrix [][]int, maxInDir int, blockBeforeTurn int) int {
 	rows, cols := len(matrix), len(matrix[0])
 
 	visited := make(map[string]bool)
@@ -113,15 +130,14 @@ func minHeatLossDijkstra(matrix [][]int) int {
 	for i := range distance {
 		distance[i] = make([]int, cols)
 		for j := range distance[i] {
-			distance[i][j] = -1
+			distance[i][j] = math.MaxInt
 		}
 	}
 
-	// so instead of just using the position (x, y) as key in the lookup table / the queue, you need to use
-	// the position (x, y) + the previous position (x, y) + number of straight moves that took to get to the position
-	// Start from the top-left corner
 	start := &Point{0, 0, matrix[0][0], nil, 0}
 	heap.Push(&pq, start)
+	heap.Push(&pq, &Point{0, 1, matrix[0][1], start, 0})
+
 	distance[0][0] = start.heatLoss
 
 	// Directions: right, down, left, up
@@ -129,80 +145,78 @@ func minHeatLossDijkstra(matrix [][]int) int {
 
 	points := make([]*Point, 0)
 	for pq.Len() > 0 {
+		// Get the current point
 		current := heap.Pop(&pq).(*Point)
+		// If this point has already been visited, skip it
 		if visited[key(current)] {
 			continue
 		}
-
+		// Set the point to visited
+		visited[key(current)] = true
 		points = append(points, current)
-		fmt.Printf("CP (%d, %d) heatLoss: %d, key: %s\n", current.row, current.col, current.heatLoss, key(current))
 		straightMoves := straightCount(current)
-		fmt.Printf("straightMoves: %d\n", straightMoves)
+
+		// Look at the adjacent points
 		for _, dir := range directions {
 			newRow, newCol := current.row+dir[0], current.col+dir[1]
+			// Bounds check
 			if newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols {
 				continue
 			}
 			moves := straightMoves
-			if current.row == newRow || current.col == newCol {
-				moves++
-			} else {
-				moves = 0
-			}
-			newPoint := &Point{newRow, newCol, matrix[newRow][newCol], current, moves}
-			// If Point has not been seen before, add it to the queue
-			if _, ok := visited[key(newPoint)]; !ok {
-				fmt.Printf("Pushing (%d, %d) -> %s\n", newPoint.row, newPoint.col, key(newPoint))
-				heap.Push(&pq, newPoint)
-				continue
-			}
-			point := pq[pq.Get(newPoint)]
-			movedDirection := false
-			if straightMoves >= 3 {
+			moved := false
+			if moves > 3 {
 				// Cant move in the same direction, need to go left or right
 				if current.row == newRow || current.col == newCol {
 					continue
 				}
-				movedDirection = true
+				moved = true
+			}
+			if moved {
+				moves = 0
+			} else {
+				moves++
 			}
 
-			newHeatLoss := current.heatLoss + point.heatLoss
-			// Can only update the distance if the last three moves were not in the same direction
-			if distance[newRow][newCol] == -1 || newHeatLoss < distance[newRow][newCol] {
-				distance[newRow][newCol] = newHeatLoss
-				if movedDirection {
-					point.numStraightMoves = 0
+			newPoint := &Point{newRow, newCol, matrix[newRow][newCol] + current.heatLoss, current, moves}
+			// if the point is already in the queue, update the heat loss
+			idx := pq.Get(newPoint)
+			if idx != -1 {
+				if newPoint.heatLoss < pq[idx].heatLoss {
+					pq[idx].heatLoss = newPoint.heatLoss
+					pq[idx].prev = newPoint.prev
+					pq[idx].numStraightMoves = newPoint.numStraightMoves
+					heap.Fix(&pq, idx)
 				}
-				fmt.Println("HERE")
-				// A new heat loss was calculated. So we need to update the point within the queue
-				// with the new heat loss
-				idx := pq.Get(point)
-				heap.Remove(&pq, idx)
-				heap.Push(&pq, point)
 			}
 
+			// Add the point to the queue
+			heap.Push(&pq, newPoint)
+			distance[newRow][newCol] = int(math.Min(float64(distance[newRow][newCol]), float64(newPoint.heatLoss)))
 		}
 
 	}
 
-	// Print out distances map
-	for _, row := range distance {
-		fmt.Println(row)
-	}
+	// For fun print the path
+	printPath(points, matrix)
 
+	// The minimum heat loss at the bottom-right corner
+	return distance[rows-1][cols-1]
+}
+
+func printPath(points []*Point, m [][]int) {
 	// Print out the path
 	i, _ := iterators.Find(points, func(p *Point) bool {
-		return p.row == rows-1 && p.col == cols-1
+		return p.row == len(m)-1 && p.col == len(m[0])-1
 	})
+
 	current := points[i]
 	path := make([]*Point, 0)
 	for current != nil {
-		// fmt.Printf("(%d, %d) -> ", current.row, current.col)
-		current = current.prev
 		path = append(path, current)
+		current = current.prev
 	}
-
-	for i, row := range matrix {
+	for i, row := range m {
 		for j, col := range row {
 			if slices.ContainsFunc(path, func(p *Point) bool {
 				return p != nil && p.row == i && p.col == j
@@ -214,9 +228,6 @@ func minHeatLossDijkstra(matrix [][]int) int {
 		}
 		fmt.Println()
 	}
-
-	// The minimum heat loss at the bottom-right corner
-	return distance[rows-1][cols-1]
 }
 
 func straightCount(current *Point) int {
@@ -224,21 +235,23 @@ func straightCount(current *Point) int {
 		return 0
 	}
 	og := current
-	count := 0
+	hCount := 0
 	for current.prev != nil {
 		if current.row == current.prev.row {
-			count++
+			hCount++
 		} else {
 			break
 		}
 		current = current.prev
 	}
 
-	if count >= 3 {
-		return count
+	if hCount >= 3 {
+		return hCount
+	} else if hCount != 0 {
+		return hCount
 	}
 
-	count = 0
+	count := 0
 	current = og
 	for current.prev != nil {
 		if current.col == current.prev.col {
@@ -248,25 +261,6 @@ func straightCount(current *Point) int {
 		}
 		current = current.prev
 	}
+
 	return count
 }
-
-// func getAdjPoints(p Point, matrix *[][]int) []Point {
-// 	directions := [][2]int{
-// 		{0, 1},  // Right
-// 		{0, -1}, // left
-// 		{1, 0},  // Down
-// 		{-1, 0}, // Up
-// 	}
-// 	adj := make([]Point, 0)
-
-// 	for _, dir := range directions {
-// 		dx, dy := p.row+dir[0], p.col+dir[1]
-// 		if dx < 0 || dx >= len(*matrix) || dy < 0 || dy >= len((*matrix)[0]) {
-// 			continue
-// 		}
-// 		adj = append(adj, Point{dx, dy, (*matrix)[dx][dy]})
-// 	}
-
-// 	return adj
-// }
