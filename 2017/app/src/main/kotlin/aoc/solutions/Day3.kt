664b36adab6d8585458d8af6f6cd467325750710
Josue Morales <molinajosue92@hotmail.com>
2024-01-16 21:13:58 -0500

    working on day 3

---
+++ 2017/app/src/main/kotlin/aoc/solutions/Day3.kt
@@ -0,0 +1,11 @@
+package aoc.solutions
+
+fun solveDayThree() {
+    val input = "1024"
+    println("Day 3 part 1: ${partOne(input)}")
+}
+
+// Manhantan distance: | x_1 - x_2 | + | y_1 - y_1 |
+private fun partOne(input: String) {
+    val grid = mutableListOf<List<Int>>()
+}
