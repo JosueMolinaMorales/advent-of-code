db8553cd64303bc4a8afd6c63a06975e4aa6e1dc
Josue Morales <molinajosue92@hotmail.com>
2024-01-27 16:08:15 -0500

    finish day 2

--- 2018/src/solutions/day_2.rs
+++ 2018/src/solutions/day_2.rs
@@ -3,7 +3,8 @@ use std::collections::{HashMap, HashSet};
 use crate::utils::file_loader::FileLoader;
 pub fn solve_day_two() {
     let input = FileLoader::new("./inputs/day2.txt".into()).read_lines();
-    println!("Day 2 part 1: {}", part_one(input))
+    println!("Day 2 part 1: {}", part_one(input.clone()));
+    println!("Day 2 part 2: {}", part_two(input))
 }
 
 fn part_one(input: Vec<String>) -> i32 {
@@ -27,8 +28,18 @@ fn part_one(input: Vec<String>) -> i32 {
 }
 
 fn part_two(input: Vec<String>) -> String {
-    let ids: Vec<HashSet<char>> = input.iter().fold(Vec::new(), |mut acc, id| {
-        acc.extend(id.chars());
-        acc
-    });
+    // What letters are common between the two correct box IDs?
+    let mut seen = HashSet::new();
+    for box_id in input.iter() {
+        for i in 0..box_id.len() {
+            let mut new_id = box_id.clone();
+            new_id.remove(i);
+            let id = format!("{}{}", i, new_id);
+            if seen.contains(&id) {
+                return new_id;
+            }
+            seen.insert(id);
+        }
+    }
+    "".into()
 }
