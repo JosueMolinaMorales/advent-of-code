ee595e2c5c2f23eaad0b6c448d4d44f6ef4e76f5
Josue Morales <molinajosue92@hotmail.com>
2024-02-07 20:04:00 -0500

    working on day 18

---
+++ 2018/src/solutions/day_14.rs
@@ -0,0 +1,19 @@
+pub fn solve_day_fourteen() {
+    let input = 0;
+    println!("Day 14 part 1: {}", part_one(input));
+    println!("Day 14 part 2: {}", part_two(input));
+}
+
+fn part_one(input: i64) -> i64 {
+    let mut elves = vec![0, 1];
+    let mut recipes = vec![3, 7];
+    let mut index = 0;
+    // Add the recipes together
+    let sum = recipes[index..].iter().sum::<i64>();
+
+    0
+}
+
+fn part_two(input: i64) -> i64 {
+    0
+}
