3c95ff9266834e524db20925aa743d6e3ea13cc8
Josue Morales <molinajosue92@hotmail.com>
2024-02-24 12:00:07 -0500

    help

--- 2018/main.py
+++ 2018/main.py
@@ -1,144 +1,170 @@
-from typing import NamedTuple
-from dataclasses import dataclass
-import enum
-import itertools
-import collections
+from itertools import count
+import fileinput
+import heapq
 
 
-class Pt(NamedTuple('Pt', [('x', int), ('y', int)])):
-    def __add__(self, other):
-        return type(self)(self.x + other.x, self.y + other.y)
+def shortest_paths(source, targets, occupied):
+    result = []
+    best = None
+    visited = set(occupied)
+    queue = [(0, [source])]
+    while queue:
+        distance, path = heapq.heappop(queue)
+        if best and len(path) > best:
+            return result
+        node = path[-1]
+        if node in targets:
+            result.append(path)
+            best = len(path)
+            continue
+        if node in visited:
+            continue
+        visited.add(node)
+        for neighbor in adjacent({node}):
+            if neighbor in visited:
+                continue
+            heapq.heappush(queue, (distance + 1, path + [neighbor]))
+    return result
 
-    @property
-    def nb4(self):
-        return [self + d for d in [Pt(0, 1), Pt(1, 0), Pt(0, -1), Pt(-1, 0)]]
+
+def manhattan_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-class Team(enum.Enum):
-    ELF = enum.auto()
-    GOBLIN = enum.auto()
+def adjacent(positions):
+    return set((y + dy, x + dx)
+               for y, x in positions
+               for dy, dx in [(-1, 0), (0, -1), (0, 1), (1, 0)])
 
 
-@dataclass
+def choose_target(position, targets, occupied):
+    if not targets:
+        return None
+    if position in targets:
+        return position
+    paths = shortest_paths(position, targets, occupied)
+    ends = [x[-1] for x in paths]
+    return min(ends) if ends else None
+
+
+def choose_move(position, target, occupied):
+    if position == target:
+        return position
+    paths = shortest_paths(position, {target}, occupied)
+    starts = [x[1] for x in paths]
+    return min(starts) if starts else None
+
+
 class Unit:
-    team: Team
-    position: Pt
-    hp: int = 200
-    alive: bool = True
-    power: int = 3
+    def __init__(self, team, position):
+        self.team = team
+        self.position = position
+        self.hp = 200
 
 
-class ElfDied(Exception):
-    pass
-
-
-class Grid(dict):
-    def __init__(self, lines, power=3):
-        super().__init__()
-
+class Model:
+    def __init__(self, lines, elf_attack=None):
+        self.elf_attack = elf_attack
+        self.walls = set()
         self.units = []
+        self.rounds = 0
+        for y, line in enumerate(lines):
+            for x, c in enumerate(line.strip()):
+                if c == '#':
+                    self.walls.add((y, x))
+                elif c in 'EG':
+                    self.units.append(Unit(c, (y, x)))
 
-        for i, line in enumerate(lines):
-            for j, el in enumerate(line):
-                self[Pt(i, j)] = el == '#'
+    def total_hp(self):
+        return sum(x.hp for x in self.units if x.hp > 0)
 
-                if el in 'EG':
-                    self.units.append(Unit(
-                        team={'E': Team.ELF, 'G': Team.GOBLIN}[el],
-                        position=Pt(i, j),
-                        power={'E': power, 'G': 3}[el]
-                    ))
+    def occupied(self, unit=None):
+        units = set(x.position for x in self.units
+                    if x != unit and x.hp > 0)
+        return self.walls | units
 
-    def play(self, elf_death=False):
-        rounds = 0
-        while True:
-            if self.round(elf_death=elf_death):
-                break
-            rounds += 1
-        print('rounds:', rounds)
-        print('units healths: ', [
-              unit.hp for unit in self.units if unit.alive])
-        return rounds * sum(unit.hp for unit in self.units if unit.alive)
-
-    def round(self, elf_death=False):
-        for unit in sorted(self.units, key=lambda unit: unit.position):
-            if unit.alive:
-                if self.move(unit, elf_death=elf_death):
-                    return True
-
-    def move(self, unit, elf_death=False):
-        targets = [target for target in self.units if unit.team !=
-                   target.team and target.alive]
-        occupied = set(
-            u2.position for u2 in self.units if u2.alive and unit != u2)
-
+    def get_move(self, unit):
+        occupied = self.occupied(unit)
+        targets = set(x.position for x in self.units
+                      if x.team != unit.team and x.hp > 0)
         if not targets:
-            return True
+            return None
+        in_range = adjacent(targets) - occupied
+        target = choose_target(unit.position, in_range, occupied)
+        if target is None:
+            return unit.position
+        move = choose_move(unit.position, target, occupied)
+        return move
 
-        in_range = set(
-            pt for target in targets for pt in target.position.nb4 if not self[pt] and pt not in occupied)
+    def get_attack(self, unit):
+        units = [(x.hp, x.position, x) for x in self.units
+                 if x.team != unit.team and x.hp > 0 and
+                 manhattan_distance(unit.position, x.position) == 1]
+        return min(units)[-1] if units else None
 
-        if not unit.position in in_range:
-            move = self.find_move(unit.position, in_range)
+    def step(self):
+        units = sorted(self.units, key=lambda x: x.position)
+        for unit in units:
+            if unit.hp <= 0:
+                continue
+            move = self.get_move(unit)
+            if move is None:
+                return False
+            unit.position = move
+            attack = self.get_attack(unit)
+            if attack:
+                if self.elf_attack:
+                    if unit.team == 'G':
+                        attack.hp -= 3
+                        if attack.hp <= 0:
+                            raise Exception
+                    else:
+                        attack.hp -= self.elf_attack
+                else:
+                    attack.hp -= 3
+        self.rounds += 1
+        return True
 
-            if move:
-                unit.position = move
+    def run(self):
+        while True:
+            if not self.step():
+                return self.rounds, self.total_hp()
 
-        opponents = [
-            target for target in targets if target.position in unit.position.nb4]
-
-        if opponents:
-            target = min(opponents, key=lambda unit: (unit.hp, unit.position))
-
-            target.hp -= unit.power
-
-            if target.hp <= 0:
-                target.alive = False
-                if elf_death and target.team == Team.ELF:
-                    raise ElfDied()
-
-    def find_move(self, position, targets):
-        visiting = collections.deque([(position, 0)])
-        meta = {position: (0, None)}
-        seen = set()
-        occupied = {unit.position for unit in self.units if unit.alive}
-
-        while visiting:
-            pos, dist = visiting.popleft()
-            for nb in pos.nb4:
-                if self[nb] or nb in occupied:
-                    continue
-                if nb not in meta or meta[nb] > (dist + 1, pos):
-                    meta[nb] = (dist + 1, pos)
-                if nb in seen:
-                    continue
-                if not any(nb == visit[0] for visit in visiting):
-                    visiting.append((nb, dist + 1))
-            seen.add(pos)
-
-        try:
-            min_dist, closest = min(
-                (dist, pos) for pos, (dist, parent) in meta.items() if pos in targets)
-        except ValueError:
-            return
-
-        while meta[closest][0] > 1:
-            closest = meta[closest][1]
-
-        return closest
+    def __str__(self):
+        units = dict((x.position, x) for x in self.units if x.hp > 0)
+        x0 = min(x for y, x in self.walls)
+        x1 = max(x for y, x in self.walls)
+        y0 = min(y for y, x in self.walls)
+        y1 = max(y for y, x in self.walls)
+        rows = []
+        for y in range(y0, y1 + 1):
+            row = []
+            row_units = []
+            for x in range(x0, x1 + 1):
+                c = '#' if (y, x) in self.walls else '.'
+                unit = units.get((y, x))
+                if unit:
+                    c = unit.team
+                    row_units.append(unit)
+                row.append(c)
+            row.append('   ')
+            row.append(', '.join('%s(%d)' % (unit.team, unit.hp)
+                                 for unit in row_units))
+            rows.append(''.join(row))
+        return '\n'.join(rows) + '\n'
 
 
-lines = open('inputs/day15.txt').read().splitlines()
+lines = list(fileinput.input())
 
-grid = Grid(lines)
+# part 1
+rounds, hp = Model(lines).run()
+print(rounds * hp)
 
-print('part 1:', grid.play())
-
-for power in itertools.count(4):
+# part 2
+for elf_attack in count(4):
     try:
-        outcome = Grid(lines, power).play(elf_death=True)
-    except ElfDied:
-        continue
-    else:
-        print('part 2:', outcome)
+        rounds, hp = Model(lines, elf_attack).run()
+        print(rounds * hp)
         break
+    except Exception:
+        pass
