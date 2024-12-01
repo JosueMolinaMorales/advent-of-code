94313afa1fa928c82858222869a10c52d89e3e3b
Josue Molina Morales <molinajosue92@hotmail.com>
2024-12-01 07:16:12 -0500

    finish day 1

---
+++ 2024/internal/days/one/day_1.go
@@ -0,0 +1,85 @@
+package one
+
+import (
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/JosueMolinaMorales/aoc/2024/internal/util"
+)
+
+func Solve_day_1() {
+	rawLists, err := util.LoadFileAsString("./inputs/day_1.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	res := solve_part_one(rawLists)
+	fmt.Println("Day 1 Part 1: ", res)
+	res = solve_part_two(rawLists)
+	fmt.Println("Day 1 Part 2: ", res)
+}
+
+func setup(rawLists string) (leftList []int, rightList []int) {
+	// Split the lists into two
+	for _, locations := range strings.Split(rawLists, "\n") {
+		l := strings.Split(locations, "   ")
+		l[0] = strings.Trim(l[0], " ")
+		l[1] = strings.Trim(l[1], " ")
+
+		leftLocation, err := strconv.Atoi(l[0])
+		if err != nil {
+			panic(err)
+		}
+
+		rightLocation, err := strconv.Atoi(l[1])
+		if err != nil {
+			panic(err)
+		}
+
+		leftList = append(leftList, leftLocation)
+		rightList = append(rightList, rightLocation)
+	}
+
+	return leftList, rightList
+}
+
+func solve_part_one(rawLists string) int {
+	leftList, rightList := setup(rawLists)
+	// Sort both lists
+	slices.Sort(leftList)
+	slices.Sort(rightList)
+
+	// Compare and get the differences
+	n := len(leftList)
+	res := 0
+
+	for i := 0; i < n; i++ {
+		dif := leftList[i] - rightList[i]
+		res += int(math.Abs(float64(dif)))
+	}
+
+	return res
+}
+
+func solve_part_two(rawLists string) int {
+	leftList, rightList := setup(rawLists)
+
+	rep := make(map[int]int)
+	for _, loc := range rightList {
+		if rep[loc] == 0 {
+			rep[loc] = 1
+		} else {
+			rep[loc] += 1
+		}
+	}
+
+	res := 0
+	for _, loc := range leftList {
+		res += loc * rep[loc]
+	}
+
+	return res
+}
--- 2024/main.go
+++ 2024/main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/JosueMolinaMorales/aoc/2024/internal/days/one"
+
 func main() {
+	one.Solve_day_1()
 }
