659ccd3e802d0413142ae4fd5f0764463f56513c
Josue Morales <molinajosue92@hotmail.com>
2024-12-02 08:29:19 -0500

    finish day 2

--- 2024/internal/days/two/day_2.go
+++ 2024/internal/days/two/day_2.go
@@ -2,6 +2,8 @@ package two
 
 import (
 	"fmt"
+	"math"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -10,10 +12,12 @@ import (
 
 func Solve_day_2() {
 	res := solve_part_one()
-	fmt.Println(res)
+	fmt.Println("Day 2 Part 1: ", res)
+	res = solve_part_two()
+	fmt.Println("Day 2 Part 2: ", res)
 }
 
-func solve_part_one() int {
+func setup() [][]int {
 	rawReports, err := util.LoadFileAsString("./inputs/day_2.txt")
 	if err != nil {
 		panic(err)
@@ -35,7 +39,67 @@ func solve_part_one() int {
 		reports = append(reports, report)
 	}
 
-	fmt.Println(reports)
+	return reports
+}
 
+func solve_part_one() int {
+	reports := setup()
+	res := 0
+	for _, report := range reports {
+		if isSafe(report) {
+			res += 1
+		}
+	}
+	return res
+}
+
+func solve_part_two() int {
+	reports := setup()
+	res := 0
+	for _, report := range reports {
+		if isSafeRemove(report) {
+			res += 1
+		}
+	}
+	return res
+}
+
+func direction(x int) int {
+	if x < 0 {
+		return 1
+	} else if x > 0 {
+		return -1
+	}
 	return 0
+}
+
+func isSafe(report []int) bool {
+	// check the first numbers
+	dir := direction(report[0] - report[1])
+	for i := 0; i < len(report)-1; i++ {
+		diff := report[i] - report[i+1]
+		newDir := direction(diff)
+		if dir != newDir {
+			return false
+		}
+		diff = int(math.Abs(float64(diff)))
+		if diff <= 0 || diff > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafeRemove(report []int) bool {
+	if isSafe(report) {
+		return true
+	}
+
+	for i := 0; i < len(report); i++ {
+		portion := slices.Delete(slices.Clone(report), i, i+1)
+		if isSafe(portion) {
+			return true
+		}
+	}
+	return false
 }
