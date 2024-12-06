889a428154e2e0e3e6b1f2dfc20307a132d49687
Josue Morales <molinajosue92@hotmail.com>
2024-12-06 08:34:07 -0500

    clean up

--- 2024/internal/days/six/day_6.go
+++ 2024/internal/days/six/day_6.go
@@ -9,192 +9,154 @@ import (
 	"github.com/emirpasic/gods/sets/hashset"
 )
 
-type guard struct {
-	direction types.Direction
-	loc       *types.Vector
+type Guard struct {
+	Direction types.Direction
+	Location  types.Vector
 }
 
-func newGuard(dir types.Direction, loc *types.Vector) *guard {
-	return &guard{
-		direction: dir,
-		loc:       loc,
+func NewGuard(dir types.Direction, loc types.Vector) Guard {
+	return Guard{
+		Direction: dir,
+		Location:  loc,
 	}
 }
 
-func (g *guard) CopyLocation() *types.Vector {
-	return types.NewVector(g.loc.X, g.loc.Y)
+func (g *Guard) CopyLocation() *types.Vector {
+	return types.NewVector(g.Location.X, g.Location.Y)
 }
 
 func SolveDay6() {
-	res := solvePartOne()
-	fmt.Println(res)
-	res = solvePartTwo()
-	fmt.Println(res)
+	fmt.Println("Day 6 Part 1: ", solvePartOne())
+	fmt.Println("Day 6 Part 2: ", solvePartTwo())
 }
 
-func setup() (*guard, *hashset.Set, int, int) {
+func setup() (Guard, *hashset.Set, int, int) {
 	rawMap, err := util.LoadFileAsString("./inputs/day_6.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	// grid := make([][]string, 0)
-	var guard *guard
+	var guard Guard
 	blocks := hashset.New()
+	xBound, yBound := 0, 0
 
-	xBound := 0
-	yBound := 0
 	for i, row := range strings.Split(rawMap, "\n") {
-		xBound = i
-		// r := make([]string, 0)
+		xBound = i + 1
 		for j, col := range strings.Split(row, "") {
-			yBound = j
+			yBound = j + 1
 			vec := types.NewVector(i, j)
-			var direction types.Direction
+
 			switch col {
 			case ">":
-				direction = types.DIRECTION_EAST
+				guard = NewGuard(types.DIRECTION_EAST, *vec)
 			case "<":
-				direction = types.DIRECTION_WEST
+				guard = NewGuard(types.DIRECTION_WEST, *vec)
 			case "^":
-				direction = types.DIRECTION_NORTH
+				guard = NewGuard(types.DIRECTION_NORTH, *vec)
 			case "v":
-				direction = types.DIRECTION_SOUTH
+				guard = NewGuard(types.DIRECTION_SOUTH, *vec)
 			case "#":
 				blocks.Add(*vec)
-				continue
-			default:
-				continue
 			}
-			guard = newGuard(direction, vec)
 		}
 	}
-	xBound++
-	yBound++
 
 	return guard, blocks, xBound, yBound
 }
 
-func solvePartOne() int {
-	guard, blocks, xBound, yBound := setup()
+func findPaths(g Guard, blocks *hashset.Set, xBound, yBound int, findCycle bool) (*hashset.Set, *hashset.Set, bool) {
+	visited := hashset.New()
 	path := hashset.New()
-	// Stop when the guard's location is off the map
-	for guard.loc.X < xBound && guard.loc.X >= 0 && guard.loc.Y < yBound && guard.loc.Y >= 0 {
-		// Count the guards location
-		path.Add(*guard.CopyLocation())
+
+	for {
+		state := Guard{
+			Location:  *g.CopyLocation(),
+			Direction: g.Direction,
+		}
+
+		// Stop if guard's location is off the map
+		if g.Location.X < 0 || g.Location.X >= xBound || g.Location.Y < 0 || g.Location.Y >= yBound {
+			return visited, path, false
+		}
+
+		if findCycle && visited.Contains(state) {
+			// Found a cycle
+			return visited, path, true
+		}
+
+		visited.Add(state)
+		path.Add(*g.CopyLocation())
 
 		// Move the guard
-		moveGuard(guard, blocks)
+		g = moveGuard(g, blocks)
 	}
+}
 
+func solvePartOne() int {
+	guard, blocks, xBound, yBound := setup()
+	_, path, _ := findPaths(guard, blocks, xBound, yBound, false)
 	return path.Size()
 }
 
 func solvePartTwo() int {
 	guard, blocks, xBound, yBound := setup()
-	gSp := guard.CopyLocation()
-	gsd := guard.direction
-	cycles := 0
-	// Loop through the entire grid and try to place an obstacle
-	// Dont place an obstacle where the is already one and dont place one on the starting position of the guard
-	for i := 0; i < xBound; i++ {
-		for j := 0; j < yBound; j++ {
-			// Place a block, if there is already a block or if its the start pos of guard, skip
-			vec := *types.NewVector(i, j)
-			if blocks.Contains(vec) || (vec.X == gSp.X && vec.Y == gSp.Y) {
-				continue
-			}
-			// Add block
-			blocks.Add(vec)
-			// Check to see if there is a cycle
-			keepGoing := true
-			cycleDetected := true
-			path := hashset.New()
-			for keepGoing {
-				// If the g is out of the map, no cycle
-				if guard.loc.X >= xBound || guard.loc.X < 0 || guard.loc.Y >= yBound || guard.loc.Y < 0 {
-					// fmt.Println("out of bounds")
-					cycleDetected = false
-					break
-				}
-				// check to see if the guard has already been to this location with the current direction
-				state := struct {
-					pos types.Vector
-					dir types.Direction
-				}{
-					pos: *types.NewVector(guard.loc.X, guard.loc.Y),
-					dir: guard.direction,
-				}
-				if path.Contains(state) {
-					// fmt.Println("We have been to this position before: ", state, vec)
-					cycleDetected = true
-					break
-				}
-				path.Add(state)
 
-				// Move guard
-				moveGuard(guard, blocks)
-			}
-			if cycleDetected {
-				cycles++
-			}
-			// Remove it
-			blocks.Remove(vec)
-			// Reset guards position
-			guard.direction = gsd
-			guard.loc.X = gSp.X
-			guard.loc.Y = gSp.Y
+	visited, _, _ := findPaths(guard, blocks, xBound, yBound, false)
+	visited.Remove(guard.Location)
+
+	attempted := hashset.New()
+	cycleCount := 0
+
+	for _, item := range visited.Values() {
+		block := item.(Guard).Location
+		if attempted.Contains(block) {
+			continue
 		}
+
+		attempted.Add(block)
+		blocks.Add(block)
+
+		_, _, found := findPaths(guard, blocks, xBound, yBound, true)
+		if found {
+			cycleCount++
+		}
+
+		blocks.Remove(block)
 	}
 
-	return cycles
+	return cycleCount
 }
 
-func moveGuard(guard *guard, blocks *hashset.Set) {
-	dx, dy := guard.loc.X, guard.loc.Y
-	switch guard.direction {
+func moveGuard(g Guard, blocks *hashset.Set) Guard {
+	dx, dy := g.Location.X, g.Location.Y
+
+	switch g.Direction {
 	case types.DIRECTION_EAST:
-		dy += 1
+		dy++
 	case types.DIRECTION_NORTH:
-		dx -= 1
+		dx--
 	case types.DIRECTION_SOUTH:
-		dx += 1
+		dx++
 	case types.DIRECTION_WEST:
-		dy -= 1
+		dy--
 	}
 
-	// if guard is on a block, move back and change direction
+	// If the guard hits a block, change direction
 	if blocks.Contains(*types.NewVector(dx, dy)) {
-		switch guard.direction {
+		switch g.Direction {
 		case types.DIRECTION_EAST:
-			guard.direction = types.DIRECTION_SOUTH
+			g.Direction = types.DIRECTION_SOUTH
 		case types.DIRECTION_NORTH:
-			guard.direction = types.DIRECTION_EAST
+			g.Direction = types.DIRECTION_EAST
 		case types.DIRECTION_SOUTH:
-			guard.direction = types.DIRECTION_WEST
+			g.Direction = types.DIRECTION_WEST
 		case types.DIRECTION_WEST:
-			guard.direction = types.DIRECTION_NORTH
+			g.Direction = types.DIRECTION_NORTH
 		}
-		return
+		return g
 	}
 
-	guard.loc.X = dx
-	guard.loc.Y = dy
-}
-
-func printMap(gLoc types.Vector, blocks hashset.Set, xBound, yBound int) {
-	for i := 0; i < xBound; i++ {
-		for j := 0; j < yBound; j++ {
-			v := *types.NewVector(i, j)
-			if gLoc.X == i && gLoc.Y == j {
-				fmt.Print("G")
-			} else if blocks.Contains(v) {
-				fmt.Print("#")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Println("==============================")
+	g.Location.X = dx
+	g.Location.Y = dy
+	return g
 }
