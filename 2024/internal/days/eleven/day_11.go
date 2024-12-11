e0f279f3b7b18cb5b2b5a357ed5f9fe41a8324fb
Josue Molina Morales <molinajosue92@hotmail.com>
2024-12-11 08:30:14 -0500

    clean up

--- 2024/internal/days/eleven/day_11.go
+++ 2024/internal/days/eleven/day_11.go
@@ -10,103 +10,33 @@ import (
 )
 
 func SolveDay11() {
-	fmt.Println(solvePartOne())
+	fmt.Println(solve(25))
+	fmt.Println(solve(75))
 }
 
-func rockMovement(stones []int, movements int, cache_0 map[int]int, cache_1 map[int]int) ([]int, int) {
-	count := 0
-	// fmt.Printf("stones ---> %v\n", stones)
-	for i := 0; i < movements; i++ {
-		n := len(stones)
-		splits := 0
-		for j := 0; j < n+splits; j++ {
-			stone := stones[j]
-			// fmt.Printf("stone is: %d at %d\n", stone, i+1)
-			s := strconv.Itoa(stone)
-			// if stone is == 0 then -> 1
-			if (stone == 0 || stone == 1) && (cache_0 != nil && cache_1 != nil) {
-				// Remove
-				stones = append(stones[:j], stones[j+1:]...)
-				// fmt.Printf("after removal: %v\n", stones)
-				splits--
-				j--
-				if stone == 0 {
-					// fmt.Println("DIGGING THOUGH CACHE 0 FOUND: ", cache_0[movements-i-1])
-					count += cache_0[movements-i-1]
-				}
-				if stone == 1 {
-					// fmt.Println("DIGGING THOUGH CACHE 1 FOUND: ", cache_1[movements-i-1])
-					count += cache_1[movements-i-1]
-				}
-
-				continue
-			}
-			if stone == 0 {
-				stones[j] = 1
-			} else if stone >= 10 && len(s)%2 == 0 {
-				// fmt.Printf("Stone is to be split: %d\n", stone)
-				// if stone is >= 10 and even split into two
-				s1 := util.ToInt(s[:len(s)/2])
-				// fmt.Printf("	first have of stone is %d\n", s1)
-				s2 := util.ToInt(s[len(s)/2:])
-				// fmt.Printf("	second have of stone is %d\n", s2)
-				stones = append(stones[:j], append([]int{s1, s2}, stones[j+1:]...)...)
-				j += 1
-				splits++
-				continue
-			} else {
-				// otherwise stone * 2024
-				// fmt.Println("Stone to be multiplied by 2024")
-				stones[j] = stone * 2024
-			}
-
-		}
-
-		// fmt.Printf("After blink %d: %v\n", i+1, stones)
-		// fmt.Printf("After blink %d: %d\n", i+1, len(stones))
-		// fmt.Println(len(stones))
+func setup() []int {
+	rawStones, err := util.LoadFileAsString("./inputs/day_11.txt")
+	if err != nil {
+		panic(err)
 	}
-	// fmt.Printf("AFTER EVERYTHING: COUNT: %d - LEN(): %d\n", count, len(stones))
-	return stones, len(stones) + count
-}
-
-func solvePartOne() int {
-	// rawStones, err := util.LoadFileAsString("./inputs/day_11.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	rawStones := "773 79858 0 71 213357 2937 1 3998391"
 	stones := []int{}
 	for _, s := range strings.Split(rawStones, " ") {
 		stones = append(stones, util.ToInt(s))
 	}
+	return stones
+}
 
+func solve(blinks int) int {
+	stones := setup()
 	count := 0
 	memo := map[types.Vector]int{}
 	for _, num := range stones {
-		count += solve(num, 75, memo)
+		count += getRockCount(num, blinks, memo)
 	}
-
-	// cache_0 := map[int]int{}
-	// s_0 := []int{0}
-	// for i := 0; i < 75; i++ {
-	// 	s_0, cache_0[i] = rockMovement(s_0, 1, nil, nil)
-	// }
-	// // fmt.Println(cache_0)
-	// fmt.Println("Part 1 done")
-	// cache_1 := map[int]int{}
-	// s_1 := []int{1}
-	// for i := 0; i < 75; i++ {
-	// 	s_1, cache_1[i] = rockMovement(s_1, 1, nil, nil)
-	// }
-	// // fmt.Println(cache_1)
-	// fmt.Println("part 2 done")
-	// _, res := rockMovement(stones, 75, cache_0, cache_1)
-
 	return count
 }
 
-func solve(num int, blinks int, memo map[types.Vector]int) int {
+func getRockCount(num int, blinks int, memo map[types.Vector]int) int {
 	if blinks == 0 {
 		return 1
 	}
@@ -117,7 +47,7 @@ func solve(num int, blinks int, memo map[types.Vector]int) int {
 
 	count := 0
 	for _, stone := range blink(num) {
-		count += solve(stone, blinks-1, memo)
+		count += getRockCount(stone, blinks-1, memo)
 	}
 	memo[*types.NewVector(num, blinks)] = count
 
@@ -131,10 +61,7 @@ func blink(num int) []int {
 
 	s := strconv.Itoa(num)
 	if len(s)%2 == 0 {
-		// fmt.Printf("Stone is to be split: %d\n", stone)
-		// if stone is >= 10 and even split into two
 		s1 := util.ToInt(s[:len(s)/2])
-		// fmt.Printf("	first have of stone is %d\n", s1)
 		s2 := util.ToInt(s[len(s)/2:])
 
 		return []int{s1, s2}
