1f67df797894e5044fa60f20500747bd88f98e92
Josue Molina Morales <molinajosue92@hotmail.com>
2024-12-15 17:16:12 -0500

    finish day 15

--- 2024/internal/days/fifteen/day_15.go
+++ 2024/internal/days/fifteen/day_15.go
@@ -2,6 +2,7 @@ package fifteen
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/JosueMolinaMorales/aoc/2024/internal/util"
@@ -9,7 +10,8 @@ import (
 )
 
 func SolveDay15() {
-	fmt.Println(solvePartOne())
+	fmt.Println("Day 15 Part 1: ", solvePartOne())
+	fmt.Println("Day 15 Part 2: ", solvePartTwo())
 }
 
 func setup() ([][]string, []string, types.Vector) {
@@ -65,6 +67,43 @@ func solvePartOne() int {
 	return res
 }
 
+func solvePartTwo() int {
+	// Enlarge map
+	m, moves, robot := setup()
+	newMap := [][]string{}
+	for _, row := range m {
+		str := strings.Join(row, "")
+		str = strings.ReplaceAll(str, "#", "##")
+		str = strings.ReplaceAll(str, "O", "[]")
+		str = strings.ReplaceAll(str, ".", "..")
+		newMap = append(newMap, strings.Split(str, ""))
+	}
+
+	robot.Y *= 2
+	for _, move := range moves {
+		switch move {
+		case "<":
+			moveRobotHorizontally(&robot, &newMap, types.DIRECTION_WEST)
+		case ">":
+			moveRobotHorizontally(&robot, &newMap, types.DIRECTION_EAST)
+		case "v":
+			moveRobotVertically(&robot, &newMap, types.DIRECTION_SOUTH)
+		case "^":
+			moveRobotVertically(&robot, &newMap, types.DIRECTION_NORTH)
+		}
+	}
+
+	res := 0
+	for i, row := range newMap {
+		for j, col := range row {
+			if col == "[" {
+				res += calcGPS(i, j)
+			}
+		}
+	}
+	return res
+}
+
 func calcGPS(x, y int) int {
 	return 100*x + y
 }
@@ -101,6 +140,117 @@ func moveRobot(robot *types.Vector, m *[][]string, direction types.Direction) {
 			continue
 		}
 
+		// If its a empty spot, move everything
+		dv := *types.NewVector(dx, dy)
+		for i := 0; i < toMove; i++ {
+			// Swap
+			(*m)[dv.X][dv.Y] = (*m)[dv.X-move.X][dv.Y-move.Y]
+			(*m)[dv.X-move.X][dv.Y-move.Y] = "."
+			dv.X -= move.X
+			dv.Y -= move.Y
+		}
+		// Move the robot
+		robot.X += move.X
+		robot.Y += move.Y
+		return
+	}
+}
+
+func moveBoxesVertically(currBox []int, m [][]string, yDir int) ([][]int, bool) {
+	// Check to the left and right for a box
+	dx_1, dy_1, dx_2, dy_2 := currBox[0]+yDir, currBox[1], currBox[0]+yDir, currBox[2]
+	// Check if there is a wall
+	cell_1 := m[dx_1][dy_1]
+	cell_2 := m[dx_2][dy_2]
+	if cell_1 == "#" {
+		return nil, false
+	}
+	if cell_2 == "#" {
+		return nil, false
+	}
+
+	boxes := [][]int{}
+	if cell_1 == "[" {
+		boxes = append(boxes, []int{dx_1, dy_1, dy_1 + 1})
+	}
+	if cell_1 == "]" {
+		boxes = append(boxes, []int{dx_1, dy_1 - 1, dy_1})
+	}
+	if cell_2 == "[" {
+		boxes = append(boxes, []int{dx_2, dy_2, dy_2 + 1})
+	}
+	if cell_2 == "]" && cell_1 != "[" {
+		boxes = append(boxes, []int{dx_2, dy_2 - 1, dy_2})
+	}
+
+	return boxes, true
+}
+
+func moveRobotVertically(robot *types.Vector, m *[][]string, direction types.Direction) {
+	move := movement(direction)
+	dx, dy := robot.X+move.X, robot.Y
+	boxesToMove := [][]int{}
+	if (*m)[dx][dy] == "#" {
+		return
+	}
+	// Check if there are any boxes on this level
+	if (*m)[dx][dy] == "]" {
+		boxesToMove = append(boxesToMove, []int{dx, dy - 1, dy})
+	}
+	if (*m)[dx][dy] == "[" {
+		boxesToMove = append(boxesToMove, []int{dx, dy, dy + 1})
+	}
+	// Need to check above each of the boxes
+	n := len(boxesToMove)
+	for i := 0; i < n; i++ {
+		box := boxesToMove[i]
+		boxes, canMove := moveBoxesVertically(box, *m, move.X)
+		if !canMove {
+			return
+		}
+		boxesToMove = append(boxesToMove, boxes...)
+		n += len(boxes)
+	}
+
+	// Move all the boxes
+	if direction == types.DIRECTION_NORTH {
+		slices.SortFunc(boxesToMove, func(a, b []int) int {
+			return a[0] - b[0]
+		})
+	}
+	if direction == types.DIRECTION_SOUTH {
+		slices.SortFunc(boxesToMove, func(a, b []int) int {
+			return b[0] - a[0]
+		})
+	}
+
+	for _, box := range boxesToMove {
+		dx, dy := box[0]+move.X, box[1] // The '['
+		(*m)[dx][dy] = "["
+		(*m)[dx][dy+1] = "]"
+		(*m)[box[0]][box[1]] = "."
+		(*m)[box[0]][box[2]] = "."
+	}
+	// Move the robot
+	robot.X += move.X
+}
+
+func moveRobotHorizontally(robot *types.Vector, m *[][]string, direction types.Direction) {
+	move := movement(direction)
+	dx, dy := robot.X+move.X, robot.Y+move.Y
+	toMove := 0
+	for {
+		// Check for wall
+		if (*m)[dx][dy] == "#" {
+			return
+		}
+		cell := (*m)[dx][dy]
+		// check if its part of a box
+		if cell == "[" || cell == "]" {
+			toMove += 2
+			dy += move.Y * 2
+			continue
+		}
 		// If its a empty spot, move everything
 		dv := *types.NewVector(dx, dy)
 		for i := 0; i < toMove; i++ {
