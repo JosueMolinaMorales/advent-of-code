2e6f7d7cd2b74e27fdc8e60290f7f3914e4644fd
Josue Morales <molinajosue92@hotmail.com>
2024-12-19 12:13:30 -0500

    finish day 19

--- 2024/internal/days/nineteen/day_19.go
+++ 2024/internal/days/nineteen/day_19.go
@@ -2,19 +2,18 @@ package nineteen
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/JosueMolinaMorales/aoc/2024/internal/util"
-	"github.com/emirpasic/gods/sets/hashset"
+	"github.com/JosueMolinaMorales/aoc/2024/internal/util/iterators"
 )
 
 func SolveDay19() {
-	fmt.Println(solvePartOne())
+	fmt.Println("Day 19 Part 1: ", solvePartOne())
+	fmt.Println("Day 19 Part 2: ", solvePartTwo())
 }
 
-// TOO HIGH: 327
-func solvePartOne() int {
+func setup() ([]string, []string) {
 	input, err := util.LoadFileAsString("./inputs/day_19.txt")
 	if err != nil {
 		panic(err)
@@ -24,39 +23,42 @@ func solvePartOne() int {
 	towels := strings.Split(parts[0], ", ")
 	designs := strings.Split(parts[1], "\n")
 
-	// fmt.Println(towels)
-	// fmt.Println(designs)
+	return towels, designs
+}
 
+func solvePartOne() int {
+	towels, designs := setup()
+
+	return len(iterators.Filter(designs, func(design string) bool {
+		return makeDesign(design, towels, &map[string]int{}) > 0
+	}))
+}
+
+func solvePartTwo() int {
+	towels, designs := setup()
 	count := 0
 	for _, design := range designs {
-		indices := hashset.New()
-		fmt.Printf("Looking at design: %s\n", design)
-		// c := map[string]int{}
-		// for _, ch := range design {
-		// 	c[string(ch)]++
-		// }
-		// d := map[string]int{}
-		for _, towel := range towels {
-			fmt.Printf(" looking at towel: %s\n", towel)
-			re, err := regexp.Compile(fmt.Sprintf("(%s)+", towel))
-			if err != nil {
-				panic(err)
-			}
-
-			idxs := re.FindAllIndex([]byte(design), -1)
-			fmt.Println("  idxs: ", idxs)
-			for _, ra := range idxs {
-				for i := ra[0]; i < ra[1]; i++ {
-					fmt.Println("  Adding: ", i)
-					indices.Add(i)
-				}
-			}
-		}
-		if indices.Size() == len(design) {
-			fmt.Println("MATCH!")
-			count++
-		}
-		fmt.Println()
+		count += makeDesign(design, towels, &map[string]int{})
 	}
 	return count
+}
+
+func makeDesign(design string, patterns []string, cache *map[string]int) int {
+	if len(design) == 0 {
+		return 1
+	}
+	if val, ok := (*cache)[design]; ok {
+		return val
+	}
+
+	filtered := iterators.Filter(patterns, func(p string) bool {
+		return strings.HasPrefix(design, p)
+	})
+
+	sum := 0
+	for _, f := range filtered {
+		sum += makeDesign(strings.TrimPrefix(design, f), patterns, cache)
+	}
+	(*cache)[design] = sum
+	return sum
 }
---
+++ 2024/internal/util/iterators/functions.go
@@ -0,0 +1,109 @@
+package iterators
+
+func IndexOf[T comparable](list []T, element T) int {
+	for i, v := range list {
+		if v == element {
+			return i
+		}
+	}
+	return -1
+}
+
+// Filter filters a list based on a predicate
+func Filter[T any](list []T, f func(T) bool) []T {
+	var filtered []T
+	for _, v := range list {
+		if f(v) {
+			filtered = append(filtered, v)
+		}
+	}
+
+	return filtered
+}
+
+// Map maps a list based on a function
+func Map[T any](list []T, f func(T) T) []T {
+	var result []T
+	for _, v := range list {
+		result = append(result, f(v))
+	}
+	return result
+}
+
+func Product[T ~int | ~float32 | ~float64](list []T) T {
+	var result T = 1
+	for _, v := range list {
+		result *= v
+	}
+	return result
+}
+
+func Every[T any](list []T, f func(T) bool) bool {
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Some[T any](list []T, f func(T) bool) bool {
+	for _, v := range list {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func Repeat[T any](element T, times int) []T {
+	var result []T
+	for i := 0; i < times; i++ {
+		result = append(result, element)
+	}
+	return result
+}
+
+func Contains[T comparable](list []T, element T) bool {
+	for _, v := range list {
+		if v == element {
+			return true
+		}
+	}
+	return false
+}
+
+func Find[T any](list []T, f func(T) bool) (int, *T) {
+	for i, v := range list {
+		if f(v) {
+			return i, &v
+		}
+	}
+	return -1, nil
+}
+
+func Sum[T ~int | ~float32 | ~float64](list []T) T {
+	var result T = 0
+	for _, v := range list {
+		result += v
+	}
+	return result
+}
+
+func Max[T ~int | ~float32 | ~float64](list []T) T {
+	var result T = list[0]
+	for _, v := range list {
+		if v > result {
+			result = v
+		}
+	}
+	return result
+}
+
+func Reverse[T any](list []T) []T {
+	var result []T
+	for i := len(list) - 1; i >= 0; i-- {
+		result = append(result, list[i])
+	}
+	return result
+}
